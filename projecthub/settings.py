77b07542870a0fbb9b223a651e9abceb8c838a64
ShariaHabib <59577264+ShariaHabib@users.noreply.github.com>
2022-03-06 00:20:15 +0600

    Update settings.py

--- projecthub/settings.py
+++ projecthub/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =''
+SECRET_KEY = 'django-insecure-sch+w#%k(lg&kh=zo++%z2l8o5qny9=!7&*e$ti^z-lx@kc9re'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
